865634cd92c2f41f7840905a3092848fabbb2f79
e1shod <hackergemer9@gmail.com>
2025-12-12 23:48:03 +0500

    vazifalar

---
+++ UyIshilar/UyIshi1.1/Uyishi1.1/Program.cs
@@ -0,0 +1,373 @@
+﻿namespace Uyishi1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-misol
+            //int soat = 2;
+            //int daqiqa = soat * 60;
+            //Console.WriteLine(soat+"soat"+           daqiqa+"daqiqaga teng");
+
+            //2-misol
+            //int hafta = 5;
+            //int kun = hafta * 7;
+            //int soat = kun * 24;
+            //Console.WriteLine(hafta +"hafta"+soat + "soatga teng");
+
+
+            //3-misol
+            //int minut = 4;
+            //int sekund = minut * 60;
+            //Console.WriteLine(minut +"Minut"+sekund+"sekundga teng");
+
+            //4-misol
+            //int soat = 25;
+            //int minut = soat * 60;
+            //int sekund = minut * 60;
+            //Console.WriteLine(soat+"soat"+sekund+"sekundga teng");
+
+            //5- misol
+            //int hafta = 150;
+            //int kun = hafta * 7;
+            //int soat = kun * 24;
+            //Console.WriteLine(hafta +"hafta"+soat + "soatga teng");
+
+
+            //6-misol
+            //int kun = 4;
+            //int soat = kun * 24;
+            //int minut = soat * 60;
+            //Console.WriteLine(kun +"kun"+minut+"minutga teng");
+
+
+            //7-misol
+            // int soat = 8;
+            //int minut = soat * 60;
+            //int sekund = minut * 60;
+            //Console.WriteLine(soat+"soat"+sekund+"sekundga teng");
+
+            //8- misol
+            //int soat = 48;
+            //int kun = soat / 24;
+            //Console.WriteLine(soat+ "saot"+kun+"kun bolad");
+
+
+            //9- misol
+            //int sekund = 3600;
+            //int minut = sekund / 60;
+            //int soat = minut / 60;
+            //Console.WriteLine(sekund+"sekund"+soat+"soat bo'ladi");
+
+
+            //10- misol
+            //int kun = 24;
+            //int hafta = kun / 7;
+            //Console.WriteLine(kun + "kun" + hafta + "hafta boladi");
+
+            //11-misol
+            //int sekunt = 2400;
+            //int minut= sekunt/60;
+            //Console.WriteLine(sekunt+"sekunt"+minut+"minutga teg");
+
+            //12-misol
+            //int sekun = 2419200;
+            //int minut = sekun / 60;
+            //int soat = minut / 60;
+            //int kun = soat / 24;
+            //int hafta = kun / 7;
+            //Console.WriteLine(sekun+"sekunt"+hafta +"haftaga teng");
+
+            //13-misol
+            //int GB = 5;
+            //int mb = GB * 1024;
+            //Console.WriteLine(GB+"gegabayt"+mb+"Megabaytga teng");
+
+
+            //14-misol
+            //int MB = 3;
+            //int kb = MB * 1024;
+            //Console.WriteLine(MB + "MB" + kb + "kb teng");#
+
+
+            //15-misol
+            //int TB= 8;
+            //int GB = TB * 1024;
+            //int mb = GB * 1024;
+            //Console.WriteLine(TB + "Terabayt" + mb + "Megabaytga teng");
+
+            //16-misol
+            //int kb = 8;
+            //int b = kb * 1024;
+            //Console.WriteLine(b);
+
+            //17-misol
+            //int mb = 4;
+            //int kb = mb * 1024;
+            //int b = kb * 1024;
+            //int bit = b * 8;
+            //Console.WriteLine(bit);
+
+
+            //18-misol
+            //int b = 3;
+            //int bit = b * 8;
+            //Console.WriteLine(bit);
+
+            //19-misol
+            //int mb = 1;
+            //int kb = mb * 1024;
+            //Console.WriteLine(kb);
+
+
+            //20-misol
+            // int tb = 8;
+            //long kb = tb * 1024L* 1024L* 1024L*1024L;
+            // Console.WriteLine(kb);
+
+
+
+            //21-misol
+            //int mb = 12;
+            //int kb = mb * 1024;
+            //int b = kb * 1024;
+            //Console.WriteLine(b);
+
+            //22-misol
+            //int gb = 5000;
+            //int tb = gb / 1024;
+            //Console.WriteLine(tb);
+
+            //23-misol
+            //int mb = 3000;
+            //int gb = mb / 1024;
+            //Console.WriteLine(gb);
+
+            //24-misol
+            //float b1 = 12000;
+            //float b = b1 / 8;
+            //float kb = b / 1024;
+            //Console.WriteLine(kb);
+
+            //25 -misol
+            //int kb = 2048;
+            //int mb= kb / 1024;
+            //Console.WriteLine(mb);
+
+            //26-misol
+            //int b1 = 16000;
+            //float b = b1 / 8;
+            //float kb = b / 1024;
+            //Console.WriteLine(kb);
+
+
+            //27-misol
+            //int b = 4096;
+            //int kb = b / 1024;
+            //Console.WriteLine(kb);
+
+
+            //28-misol
+            //int b1 = 3200;
+            //int b = b1 / 8;
+            //Console.WriteLine(b);
+
+            //29-misol
+            //float kb = 1200;
+            //float mb = kb / 1024;
+            //Console.WriteLine(mb);
+
+            //30-misol
+            //float b = 4800;
+            //float kb =b/ 1024;
+            //float mb = kb / 1024;
+            //Console.WriteLine(mb);
+
+            //31-misol
+            // int mb = 120000;
+            // float gb = mb / 1024;
+            //float tb = gb / 1024;
+            // Console.WriteLine(tb);
+
+            //32-misol
+            //int mb = 3;
+            //int kb = 8;
+            //int sum = mb * 1024 + kb;
+            //Console.WriteLine(sum);
+
+            //33-misol
+            //int gb = 7;
+            //int mb = gb *1024 +11;
+            //int kb = mb * 1024;
+            //Console.WriteLine(kb);
+
+            //34-misol;
+            //int mb = 3;
+            //int kb = mb * 1024 + 82;
+            //int b = kb * 1024;
+            //Console.WriteLine(b);
+
+            //35-misol
+            //int kb = 8;
+            //int b = kb * 1024 + 12;
+            //int b1 = b * 8;
+            //Console.WriteLine(b1);
+
+            //36-misol
+            //int mb = 4;
+            //int kb =mb*1024 + 10;
+            //int b = kb * 1024;
+            //int b1 = b * 8;
+            //Console.WriteLine(b1);
+
+
+            //37-misol
+            //Console.WriteLine(12345);
+            //Console.WriteLine(1234);
+            //Console.WriteLine(123);
+            //Console.WriteLine(12);
+            //Console.WriteLine(1);
+
+            //38-misol
+            //Console.WriteLine(1);
+            //Console.WriteLine(12);
+            //Console.WriteLine(123);
+            //Console.WriteLine(1234);
+            //Console.WriteLine(12345);
+
+
+
+            //91-misol
+            //int hafta =
+            //int.Parse(Console.ReadLine());
+            //int soat=
+            //int.Parse(Console.ReadLine());
+            //int ish1 = hafta * 7;
+            //int ish2 = soat / 24;
+            //int jami = ish1 + ish2;
+            //Console.WriteLine(jami);
+
+            //92-misol
+            //int soat =
+            //int.Parse(Console.ReadLine());
+            //int minut =
+            //int.Parse(Console.ReadLine());
+            //int sekund = soat * 3600 + minut * 60;
+            //Console.WriteLine(sekund);
+
+            //93-misol
+            // double  bayt=
+            // double.Parse(Console.ReadLine());
+            // double kb = bayt / 1024;
+            //Console.WriteLine(kb);
+
+            //94-misol
+            //double tezlik = 750;
+            //double hajmGB = 1.8;
+            //double hajmkB = hajmGB * 1024 * 1024;
+            //double vaqt = hajmGB / tezlik;
+            //Console.WriteLine(vaqt);
+
+
+            //95-msiol
+            //int dollar = 
+            //int.Parse(Console.ReadLine());
+            //int yevro =
+            //int.Parse(Console.ReadLine());
+            //int jami = dollar * 12600 + yevro * 14500;
+            //Console.WriteLine(jami);
+
+            //96 - misol
+            //int a =
+            //int.Parse(Console.ReadLine());
+            //int a2 = a * a;
+            //int a4 = a2 * a2;
+            //int a5 = a4 * a;
+            //int a10 =a5 *a5;
+            //Console.WriteLine(a5);
+
+            //97-misol
+            //int sahifa = 720;
+            //int qator = 50;
+            //int belgi = 152;
+            //int jamiBelgi = sahifa * qator * belgi;
+            //double bayt = jamiBelgi;
+            //double MB = bayt / (1024.0 * 1024.0);
+            //Console.WriteLine(MB);
+
+            //98-misol
+            //double belgi = 15;
+            //double soz = belgi / 5;
+            //double daq = soz * 2.5;
+            //Console.WriteLine(daq);
+
+            //99-misol
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //a = a + b;
+            //b = a - b;
+            //a = a - b;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //100 - misol
+            //int a = 1;
+            //int b = 2;
+            //int c = 3;
+            //int t = a;
+            //a = b;
+            //b = c;
+            //c = t;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+
+            //101-msiol
+
+            //Console.Write("Patrikni pulini kiriting ");
+            //int P =
+            //int.Parse(Console.ReadLine());
+            //Console.Write("Juliani pulini kiritng");
+            //int J =
+            //int.Parse(Console.ReadLine());
+            //int x = (P - J) / 2;
+            //Console.WriteLine(x);
+
+            //102-misol
+            //int km =
+            //int.Parse(Console.ReadLine());
+            //int m =
+            //int.Parse(Console.ReadLine());
+            //int dm = km * 10000 + m * 100;
+            //Console.WriteLine(dm);
+
+            //103-misol
+            //int a =
+            //int.Parse(Console.ReadLine());
+            //int a2 = a * a;
+            //int a4 = a2 * a2;
+            //int a5 = a4 * a;
+            //int a10 = a5 * a5;
+            //int a15 = a10 * a5;
+            //Console.WriteLine(a15);
+
+            //104 -misol            
+            //Console.Write("a ni kiriting (bino soni): ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("b ni kiriting (har binodagi qavatlar): ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.Write("c ni kiriting (har qavatdagi xona soni): ");
+            //int c = int.Parse(Console.ReadLine());
+
+            //Console.Write("d ni kiriting (har xonadagi stul soni): ");
+            //int d = int.Parse(Console.ReadLine());
+
+            //int jamiStul = a * b * c * d;
+            //int jamiParta = jamiStul / 2;
+
+        }
+    }
+}

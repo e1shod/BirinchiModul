80319e108e052c6ccf227d8ec647a535a0c54a6e
e1shod <hackergemer9@gmail.com>
2026-01-29 18:20:13 +0500

    2exem

---
+++ IkkinchiModulExam/Exem2modul/Etities/Movie.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exem2modul.Etities
+{
+    internal class Movie
+    {
+    }
+}
---
+++ IkkinchiModulExam/Exem2modul/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exem2modul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
